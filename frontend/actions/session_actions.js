34842a19c9e9fa81eb7bdfe9d2886f11c289fd76
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-08-30 16:35:05 -0700

    Completed session utils and session middleware

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,6 +1,8 @@
 export const SESSION_CONSTANTS = {
   LOGIN_USER: "LOGIN_USER",
-  CREATE_USER: "CREATE_USER"
+  CREATE_USER: "CREATE_USER",
+  RECEIVE_USER: "RECEIVE_USER",
+  RECEIVE_ERRORS: "RECEIVE_ERRORS"
 };
 
 export const createUser = (user) => ({
@@ -11,4 +13,14 @@ export const createUser = (user) => ({
 export const loginUser = (user) => ({
   type: SESSION_CONSTANTS.LOGIN_USER,
   user
+});
+
+export const receiveUser = (user) => ({
+  type: SESSION_CONSTANTS.RECEIVE_USER,
+  user
+});
+
+export const receiveErrors = (errors) => ({
+  type: SESSION_CONSTANTS.RECEIVE_ERRORS,
+  errors
 });
--- frontend/middleware/session_middleware.js
+++ frontend/middleware/session_middleware.js
@@ -1,12 +1,21 @@
 import { SESSION_CONSTANTS } from '../actions/session_actions';
 import { createUser, loginUser } from '../utils/session_middleware';
+import { receiveUser, receiveErrors } from '../actions/session_actions';
 
 const SessionMiddleware = ({getState, dispatch}) => next => action => {
+  let success;
+  let error;
   switch (action.type) {
     case SESSION_CONSTANTS.CREATE_USER:
-      break;
+      success = (data) => dispatch(receiveUser(data));
+      error = (errors) => dispatch(receiveErrors(data));
+      createUser(action.user, success, error);
+      return next(action);
     case SESSION_CONSTANTS.LOGIN_USER:
-      break;
+      success = (data) => dispatch(receiveUser(data));
+      error = (errors) => dispatch(receiveErrors(data));
+      loginUser(action.user, success, error);
+      return next(action);
     default:
       return next(action);
   }
