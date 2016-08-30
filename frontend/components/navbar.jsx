c6516ab3695a20d8fcbc06ea7060315240c8cd37
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-08-30 13:15:34 -0700

    Initial navbar done

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
 const Navbar = ({currentUser}) => {
-
+  let logged_in = (currentUser ? true : false);
+  let navButtons;
+  if (logged_in) {
+    navButtons = (
+      <ul className="nav-buttons">
+        <li>Option 1</li>
+      </ul>
+    );
+  } else {
+    navButtons = (
+      <ul className="nav-buttons">
+        <li><button>Join</button></li>
+        <li><button>Log in</button></li>
+      </ul>
+    );
+  }
   return (
-    <section class="nav-container">
-      <nav class="navbar">
-        <div class="logo">Timesurfing</div>
-        <ul class="nav-buttons">
-          <li>Option 1</li>
-        </ul>
+    <section className="nav-container">
+      <nav className="navbar">
+        <div className="logo"><a href="/#/">Timesurfing</a></div>
+        {navButtons}
       </nav>
     </section>
   );
