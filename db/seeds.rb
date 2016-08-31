fefb166c44efe0a8ae3ddc2a37691e94afad3ad8
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-08-31 10:47:34 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([{ username: "Marty", firstname: "Marty", lastname: "McFly",
+   password: "12345678", profile: "https://pbs.twimg.com/profile_images/115422070/mcfly_400x400.JPG",
+   gender: "M", birthdate: Date.new(1968, 6, 12), description: "Nobody calls me chicken" }]);
