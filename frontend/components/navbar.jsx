43041651fec2af2a1f5264bc7c7435f20923577b
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-08-31 10:25:20 -0700

    Completed styling of navbar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,41 +1,72 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 
-const Navbar = ({currentUser, logoutUser, loginUser }) => {
-  let logged_in = (currentUser ? true : false);
-  let navButtons;
-  if (logged_in) {
-    let profilePic = (currentUser.profile ? currentUser.profile : "assets/profile_default.png");
-    navButtons = (
-      <ul className="nav-buttons">
-        <li><button onClick={logoutUser} >Log out</button></li>
-        <li>
-          <a className="user-icon" href="">
-            <img src={profilePic} alt={currentUser.username} />
-          </a>
-          <ul className="navbar-dropdown">
-            <li>option 1</li>
-          </ul>
-        </li>
-      </ul>
-    );
-  } else {
-    navButtons = (
-      <ul className="nav-buttons">
-        <li><a href="/#/join"><button>Join</button></a></li>
-        <li><a href="/#/login"><button>Log in</button></a></li>
-        <li><button onClick={loginUser}>Guest</button></li>
-      </ul>
+class Navbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.iconClick = this.iconClick.bind(this);
+    this.state = {menu: false}
+  }
+
+  iconClick(e) {
+    let removeClick = () => {
+      if (this.state.menu === true) {
+        this.setState({menu: false});
+      }
+      document.removeEventListener("click", removeClick);
+    };
+    e.preventDefault();
+    if (this.state.menu === false) {
+      this.setState({menu: true});
+      document.addEventListener("click", removeClick);
+    } else {
+      removeClick();
+    }
+  }
+
+
+  render() {
+    let logged_in = (this.props.currentUser ? true : false);
+    let navButtons;
+    let navbarDropdown = (this.state.menu ? "navbar-dropdown popup" : "navbar-dropdown not-popup");
+    if (logged_in) {
+      let profilePic = (this.props.currentUser.profile ? this.props.currentUser.profile : "assets/profile_default.png");
+      navButtons = (
+        <ul className="nav-buttons">
+          <li><button onClick={this.props.logoutUser} >Log out</button></li>
+          <li>
+            <a className="user-icon" href="#" onClick={this.iconClick}>
+              <img src={profilePic} alt={this.props.currentUser.username} />
+              <i className="material-icons md-18 arrow">expand_more</i>
+            </a>
+            <ul id="menu" className={navbarDropdown} >
+              <li><a href="#">Dashboard</a></li>
+              <li><a href="#">My Profile</a></li>
+              <li><a href="#">Edit Profile</a></li>
+
+            </ul>
+          </li>
+        </ul>
+      );
+    } else {
+      navButtons = (
+        <ul className="nav-buttons">
+          <li><a href="/#/join"><button>Join</button></a></li>
+          <li><a href="/#/login"><button>Log in</button></a></li>
+          <li><button onClick={this.props.loginUser}>Guest</button></li>
+        </ul>
+      );
+    }
+    return (
+      <section className="nav-container">
+        <nav className="navbar">
+          <div className="logo"><a href="/#/"><i className="material-icons logo-icon">hourglass_empty</i> Timesurfing</a></div>
+          {navButtons}
+        </nav>
+      </section>
     );
   }
-  return (
-    <section className="nav-container">
-      <nav className="navbar">
-        <div className="logo"><a href="/#/"><i className="material-icons logo-icon">hourglass_empty</i> Timesurfing</a></div>
-        {navButtons}
-      </nav>
-    </section>
-  );
-};
+}
 
 export default withRouter(Navbar);
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -6,18 +6,35 @@ import Splash from './splash';
 import LoginContainer from './auth/login_container';
 import JoinContainer from './auth/join_container';
 
-const Root = ({store}) => (
-  <Provider store={store}>
-    <div>
-      <NavbarContainer />
-      <Router history={ hashHistory }>
-        <Route path="/" component={Splash} >
-          <Route path="/login" component={LoginContainer} />
-          <Route path="/join" component={JoinContainer} />
-        </Route>
-      </Router>
-  </div>
-  </Provider>
-);
+class Root extends React.Component {
+  constructor(props) {
+    super(props)
+    this.ensureNotLogin = this.ensureNotLogin.bind(this);
+  }
+
+  ensureNotLogin(nextState, replace) {
+    if (this.props.store.getState().session.currentUser) {
+      replace('/');
+    }
+  }
+
+  render() {
+    return (
+      <Provider store={this.props.store}>
+        <div>
+          <NavbarContainer />
+          <Router history={ hashHistory }>
+            <Route path="/" component={Splash} >
+              <Route path="/login" component={LoginContainer} onEnter={this.ensureNotLogin} />
+              <Route path="/join" component={JoinContainer} onEnter={this.ensureNotLogin} />
+            </Route>
+          </Router>
+      </div>
+      </Provider>
+    );
+  }
+}
+
+
 
 export default Root;
