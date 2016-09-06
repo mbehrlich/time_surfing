8a6f7c7e7f2201f25dee90064e093f1b09d33786
Matthew Ehrlich <mattehrlich86@gmail.com>
2016-09-06 08:58:11 -0700

    Fixed date string function on references

--- frontend/components/users/reference.jsx
+++ frontend/components/users/reference.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Reference = ({reference}) => {
-  console.log(reference);
+  let creationDate = reference.created_at.slice(0, 10).split('-');
+  creationDate = new Date(creationDate[0], creationDate[1] - 1, creationDate[2]);
   return (
     <article className="reference-item">
       <figure className="reference-pic">
@@ -11,7 +12,7 @@ const Reference = ({reference}) => {
         <section className="reference-header">
           <h4>{reference.author.firstname} {reference.author.lastname}</h4>
           <h4 className={reference.recommended ? "good-reference" : "bad-reference"} >{reference.recommended ? "Recommended" : "Not Recommended"}</h4>
-          <h5>{reference.created_at}</h5>
+          <h5>{creationDate.toDateString()}</h5>
         </section>
         <section className="reference-body">
           <p>{reference.body}</p>
